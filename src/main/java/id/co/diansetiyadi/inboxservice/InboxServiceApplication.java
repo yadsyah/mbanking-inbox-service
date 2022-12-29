6ada24eb224e999269f3a1a71b41fa94e3d74c09
yadsyah <diyansetiyadi@gmail.com>
2022-12-29 17:34:31 +0700

    init commit

---
+++ src/main/java/id/co/diansetiyadi/inboxservice/InboxServiceApplication.java
@@ -0,0 +1,13 @@
+package id.co.diansetiyadi.inboxservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InboxServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InboxServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/co/diansetiyadi/inboxservice/InboxServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.diansetiyadi.inboxservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InboxServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
