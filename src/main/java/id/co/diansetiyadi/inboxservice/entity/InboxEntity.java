882ea3db892f70e13b2bbf3196e4820b38fa11c0
yadsyah <diyansetiyadi@gmail.com>
2022-12-30 19:17:46 +0700

    feat: add validation entitiy inbox

--- src/main/java/id/co/diansetiyadi/inboxservice/entity/InboxEntity.java
+++ src/main/java/id/co/diansetiyadi/inboxservice/entity/InboxEntity.java
@@ -1,6 +1,8 @@
 package id.co.diansetiyadi.inboxservice.entity;
 
 import id.co.diansetiyadi.inboxservice.util.CategoryInboxEnum;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,10 +17,13 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @AllArgsConstructor
 public class InboxEntity extends BaseEntity {
 
+    @NotBlank(message = "field cif must not be blank!")
     private String cif;
     private String deviceId;
     private String messageDetail;
     private boolean isRead;
+    @NotBlank(message = "field category enums most nut be blank!")
+    @NotNull(message = "field category enums most nut be null!")
     private CategoryInboxEnum category;
 
 }
