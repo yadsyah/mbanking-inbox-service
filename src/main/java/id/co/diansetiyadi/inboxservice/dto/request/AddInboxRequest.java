a341827174acc5b9a75e4f9347e73ce59e87a5c4
yadsyah <diyansetiyadi@gmail.com>
2022-12-30 19:18:32 +0700

    feat: add validation request add inbox

--- src/main/java/id/co/diansetiyadi/inboxservice/dto/request/AddInboxRequest.java
+++ src/main/java/id/co/diansetiyadi/inboxservice/dto/request/AddInboxRequest.java
@@ -1,6 +1,8 @@
 package id.co.diansetiyadi.inboxservice.dto.request;
 
 import id.co.diansetiyadi.inboxservice.util.CategoryInboxEnum;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,6 +11,8 @@ import lombok.Setter;
 public class AddInboxRequest extends BaseRequest {
 
     private String messageDetail;
+    @NotBlank(message = "field cif must not be blank!")
     private String cif;
+    @NotNull(message = "field categoryInboxEnum must not be null!")
     private CategoryInboxEnum categoryInboxEnum;
 }
